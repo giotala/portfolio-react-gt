608ccbc67352496d915d57d6293fb3605308fee0
Gio Talabong <gio.talabong@gmail.com>
2020-12-15 08:40:01 -0800

    added quotes on landing page

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -16,20 +16,20 @@ export default function NavBar() {
         fontSize: "2.2rem"
     }
     return (
-        <div className="bg-transparent" style={{marginTop: "10px"}}>
-        
-        <Navbar bg="transparent"expand="lg">
-        <Navbar.Brand href="/portfolio-react-gt/" style={gio} className='text-white'>Gio Talabong</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className='text-white float-right' id="basic-navbar-nav">
-        <Nav className="" style={style}>
-        <Nav.Link href="/portfolio-react-gt/" style={fontStyle} className='text-white float-right' >About</Nav.Link>
-        <Nav.Link href="projects" style={fontStyle} className='text-white float-right'>Projects</Nav.Link>  
-        <Nav.Link href="contacts" style={fontStyle} className='text-white float-right'>Contacts</Nav.Link>  
-        </Nav>
-  </Navbar.Collapse>
-</Navbar>
+        <div className="bg-transparent" style={{ marginTop: "10px" }}>
 
-    </div>
+            <Navbar bg="transparent" expand="lg">
+                <Navbar.Brand href="/portfolio-react-gt/" style={gio} className='text-white'>Gio Talabong</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className='text-white float-right' id="basic-navbar-nav">
+                    <Nav className="" style={style}>
+                        <Nav.Link href="/portfolio-react-gt/" style={fontStyle} className='text-white float-right' >About</Nav.Link>
+                        <Nav.Link href="projects" style={fontStyle} className='text-white float-right'>Projects</Nav.Link>
+                        <Nav.Link href="contacts" style={fontStyle} className='text-white float-right'>Contacts</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+        </div>
     )
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,55 +1,64 @@
 import React from 'react';
 import { Container, Row, Col, Jumbotron, Breadcrumb } from 'react-bootstrap';
-import { ChevronRight, ChevronLeft } from 'react-bootstrap-icons';
+import { ChevronRight, ChevronLeft, EmojiExpressionless, EmojiDizzy } from 'react-bootstrap-icons';
 import Modals from './Projects/Modals'
+import emoji from './../image/emoji.png'
 // import { faFacebook } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Portfolio() {
+   
     const portStyle = {
         backgroundColor: '#ffffff6e'
     }
     return (
         <div>
-        <Container fluid className='portfolioContainer'>
-        <Row>
-        <Col></Col>
-        <Col sm={10} className="center" >
-        <Jumbotron fluid style={portStyle}>
-        
-            <h1 className='me'> <ChevronLeft> </ChevronLeft>Gio Talabong<ChevronRight /></h1>
-            <Breadcrumb >
-            <Breadcrumb.Item href="https://github.com/giotala">
-                Github
+            <Container fluid className='portfolioContainer'>
+                <Row>
+                    <Col></Col>
+                    <Col sm={10} className="center" >
+                        <Jumbotron fluid style={portStyle}>
+
+                            <h1 className='me'> <ChevronLeft> </ChevronLeft>Gio Talabong<ChevronRight /></h1>
+                            <Breadcrumb >
+                                <Breadcrumb.Item href="https://github.com/giotala">
+                                    Github
             </Breadcrumb.Item>
-            <Breadcrumb.Item href="https://www.linkedin.com/in/gio-talabong-79086b103/">
-                LinkedIn
+                                <Breadcrumb.Item href="https://www.linkedin.com/in/gio-talabong-79086b103/">
+                                    LinkedIn
             </Breadcrumb.Item>
-            {/* <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+                                {/* <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
                 Instagram
             </Breadcrumb.Item>
             <Breadcrumb.Item href="https://www.facebook.com/gio.talabong.1/">
             <i class="fas fa-facebook"></i>FB
                 <Modals/>
             </Breadcrumb.Item> */}
-            
-            </Breadcrumb>
-        </Jumbotron>
-        </Col>
-        <Col></Col>
-        </Row>
-        </Container>
-        <div class="boxes">
-        <ul class="single-box">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </div>
+
+                            </Breadcrumb>
+
+                            <div>
+                                <ChevronLeft />
+                                id
+                                <ChevronRight />
+                            You are special! <img className="emoji" src={emoji}/>
+                            </div>
+                        </Jumbotron>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
+            <div class="boxes">
+                <ul class="single-box">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
         </div>
     )
 }
